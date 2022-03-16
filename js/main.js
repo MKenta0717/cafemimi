165ee99709a64f8535847f0e6bd648432b5169bf
MKenta0717 <89497405+MKenta0717@users.noreply.github.com>
2022-03-17 02:14:58 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const menu = document.querySelector('.menu');
 const circleMenu = document.querySelector('.circle_menu');
 const circleBg = document.querySelector('.circle_bg');
+const hubMenu = document.querySelector('.hub_menu');
 
 
 menu.addEventListener('click',()=>{
   circleMenu.classList.toggle('active');
   circleBg.classList.toggle('active');
-  menu.classList.toggle('open')
+  menu.classList.toggle('open');
+  hubMenu.classList.toggle('open');
+  
  
 })
