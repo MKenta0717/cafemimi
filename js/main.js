7bea7f21418492b81c954ac34c4544a311ab7dd2
MKenta0717 <89497405+MKenta0717@users.noreply.github.com>
2022-03-20 00:24:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 const menu = document.querySelector('.menu');
 const circleMenu = document.querySelector('.circle_menu');
 const circleBg = document.querySelector('.circle_bg');
-const hubMenu = document.querySelector('.hub_menu');
+const hubMenu = document.querySelector('.hub_menu_list');
 
+//.menuをクリックした際の挙動を作るfunction
+function menuClick(target,plusClass){
+  menu.addEventListener('click',()=>{
+    target.classList.toggle(plusClass);
+  })
+}
 
-menu.addEventListener('click',()=>{
-  circleMenu.classList.toggle('active');
-  circleBg.classList.toggle('active');
-  menu.classList.toggle('open');
-  hubMenu.classList.toggle('open');
-  
- 
-})
+//.activeをつけてmenuの背景色の出現
+menuClick(circleMenu,'active');
+menuClick(circleBg,'active');
+
+//
+menuClick(menu,'open');
+menuClick(hubMenu,'open');
